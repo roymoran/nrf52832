b9696c761b444e1aa051566bd644ac905b5159ff
Roy Moran <romoran1@outlook.com>
2021-06-07 10:14:50 -0500

    enable logging over uart

--- src/main.c
+++ src/main.c
@@ -2,9 +2,25 @@
 #include <stdlib.h>
 #include "nrf_delay.h"
 #include "boards.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+/**@brief Function for handling the idle state (main loop).
+ *                                                                                                                                                                  
+ * @details Handle any pending log operation(s), then sleep until the next event occurs.
+ */
+static void idle_state_handle(void)
+{
+    NRF_LOG_FLUSH();
+}
 
 int main(void)
 {
+    /* Configure logging. */
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
     /* Configure board. */
     bsp_board_init(BSP_INIT_LEDS);
 
@@ -15,6 +31,12 @@ int main(void)
         {
             bsp_board_led_invert(i);
             nrf_delay_ms(500);
+            NRF_LOG_ERROR("logging error...");
+            NRF_LOG_DEBUG("logging debug...");
+            NRF_LOG_WARNING("logging warn...");
+            NRF_LOG_INFO("logging info...");
+            NRF_LOG_INFO("logging %d...", i);
+            idle_state_handle();
         }
     }
 }
